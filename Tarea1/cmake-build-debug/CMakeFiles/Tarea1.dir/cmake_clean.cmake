3bac70a2e2e10cb17625f09078c66f9f5fbfa086
Carlos Akion Garro Campos <carloscamp1008@estudiantec.cr>
2021-03-01 20:40:46 -0600

    Project Files

---
+++ Tarea1/cmake-build-debug/CMakeFiles/Tarea1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tarea1.dir/Collector.cpp.o"
+  "CMakeFiles/Tarea1.dir/List.cpp.o"
+  "CMakeFiles/Tarea1.dir/Node.cpp.o"
+  "CMakeFiles/Tarea1.dir/main.cpp.o"
+  "Tarea1"
+  "Tarea1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tarea1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
